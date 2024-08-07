8f2310d7e3852ab8e06d8736f9200a78c9a0696a
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-08-07 14:42:52 +0530

    project mobile version done

---
+++ App.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector('.menu-bar-btn');
+const navListDiv = document.querySelector('.nav-list-div');
+
+menuBtn.addEventListener('click', ()=>{
+    navListDiv.classList.toggle("displayNone");
+    console.log("button was clicked");   
+})
+
